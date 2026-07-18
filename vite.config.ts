5001014b2e8f96eab488d4cdc7185a7cd1496ceb
rss042code@gmail.com <rss042code@gmail.com>
2026-07-18 13:00:18 +0300

    edit#

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/',
+  base: 'https://6106.bg/',
   plugins: [
     react(),
     tailwindcss()
