4307173c992858f233d03ff3601f73e36057e0e2
rss042code@gmail.com <rss042code@gmail.com>
2026-07-18 17:37:15 +0300

    55

--- src/App.tsx
+++ src/App.tsx
@@ -31,7 +31,7 @@ function Home() {
 
 export default function App() {
   return (
-    <BrowserRouter basename="/">
+    <BrowserRouter basename="/6106.bg">
         <div className="min-h-screen bg-bg">
            <Header />
              <Routes>
