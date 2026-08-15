cb7bc10d00229fc1a13e6c52926b2ed52b8fd28a
rss042code@gmail.com <rss042code@gmail.com>
2026-08-15 07:20:31 +0300

    vite.config

--- vite.config.ts
+++ vite.config.ts
@@ -49,7 +49,7 @@ export default defineConfig({
           lang: 'bg',
           icons: [
             {
-              src: '/icon.svg',
+              src: '/favicon.svg',
               sizes: '192x192 512x512',
               type: 'image/svg+xml',
               purpose: 'any maskable'
